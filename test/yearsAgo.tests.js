dec3a80fd1b2c5d4a878376817bd332ae67934ee
Anele Silimela <silimela.anelle@gmail.com>
2021-07-25 21:37:22 +0200

    testing more functions

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -1,6 +1,10 @@
 describe('the yearsAgo function' , function(){
-    it('should take a string parameter and find out if the parameter passed in is a day of the week or not' , function(){
+    it('takes a year and returns the number of years that year is older than the current year.' , function(){
         assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976));
+        assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000));
+        assert.equal((new Date().getFullYear() - 2019), yearsAgo(2019));
+        assert.equal((new Date().getFullYear() - 1992), yearsAgo(1992));
+
         
 
     });
