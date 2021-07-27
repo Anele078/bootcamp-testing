f08d7b48cf4cb0b3c30d39fdeec3de1721a7ea49
Anele Silimela <silimela.anelle@gmail.com>
2021-07-27 18:08:48 +0200

    adding functions to the test

--- test/findItemsOver20.tests.js
+++ test/findItemsOver20.tests.js
@@ -1,13 +1,7 @@
-describe('the findItemsOver20 function' , function(){
-    it('should take a string parameter and find out if the parameter passed in is a day of the week or not' , function(){
-        assert.equal([{"name":"pears","qty":37},{"name":"bananas","qty":27}], findItemsOver20( [
-            {name : 'apples', qty : 10},
-            {name : 'pears', qty : 37},
-            {name : 'bananas', qty : 27},
-            {name : 'apples', qty : 3},
-        ]));
-        
-
+describe('The findItemsOver20 function...', function () {
+    it('should return all the products that have a quantity over 20 in the itemList', function () {
+        assert.deepEqual(results, findItemsOver20(itemList));
+        assert.deepEqual(results2, findItemsOver20(itemList2));
+        assert.deepEqual(results3, findItemsOver20(itemList3));
     });
-
 });
