d16c04fcdf6a5beecbbd7d1000850104bb431ec0
Anele Silimela <silimela.anelle@gmail.com>
2021-08-04 23:28:12 +0200

    making changes

--- test/regCheck.tests.js
+++ test/regCheck.tests.js
@@ -1,4 +1,4 @@
-describe('the regCheck' , function(){
+describe('the regCheck function ' , function(){
     it('Should check if indeed a registration number is for a particular province and return true otherwise it should return fslse' , function(){
         assert.equal(true, regCheck('DC 55 YU GP', 'GP'));
         assert.equal(false, regCheck('DC 55 YU GP','EC'));
